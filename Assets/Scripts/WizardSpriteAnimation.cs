50796fe9ab32e0791c363e1a262121220f37b817
Chris de Castro <redsun@mac.com>
2015-02-10 11:27:41 -0500

    Integrated wizard sprite animation

---
+++ Assets/Scripts/WizardSpriteAnimation.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WizardSpriteAnimation : MonoBehaviour {
+
+	public GameObject wizard;
+	Animator anim;
+	NavMeshAgent agent;
+	int xVelocityHash = Animator.StringToHash("xVelocity");
+	int dizzyHash = Animator.StringToHash("dizzy");
+	int idleHash = Animator.StringToHash("idle");
+	public string dizzyTag = "Dizzy";
+
+	void Start () {
+		anim = GetComponent<Animator>();
+		agent = wizard.GetComponent<NavMeshAgent>();
+	}
+	
+	void Update () {
+		if (wizard.CompareTag(dizzyTag)){
+			anim.SetBool(dizzyHash, true);
+		} else {
+			anim.SetBool(dizzyHash, false);
+		}
+		float xVelocity = agent.velocity.x;
+		Debug.Log(xVelocity);
+		if(xVelocity == 0f){
+			anim.SetBool(idleHash, true);
+		} else {
+			anim.SetBool(idleHash, false);
+		}
+		anim.SetFloat(xVelocityHash,xVelocity);
+	}
+}
