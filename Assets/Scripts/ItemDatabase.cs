e2e10bc77a66add19dc146a14276a477f355744f
Chris de Castro <redsun@mac.com>
2015-03-01 06:56:51 -0500

    Trees! & new beers: Amsterdam, Mill St. Organic & Creemore

--- Assets/Scripts/ItemDatabase.cs
+++ Assets/Scripts/ItemDatabase.cs
@@ -12,6 +12,9 @@ public class ItemDatabase : MonoBehaviour {
 		// beer cans use ID range 1-99
 		items.Add(new Item("Pabst", 1, "The Blue Ribbon", 5, Item.ItemType.Can, can));
 		items.Add(new Item("Steamwhistle", 2, "Green Can with the whistle", 5, Item.ItemType.Can, can));
+		items.Add(new Item("MillSt", 3, "It's Organic!", 5, Item.ItemType.Can, can));
+		items.Add(new Item("Creemore", 4, "A river runs through it...", 5, Item.ItemType.Can, can));
+		items.Add(new Item("Amsterdam", 5, "Tall & Blonde", 5, Item.ItemType.Can, can));
 
 		// recyclables use ID range 100-199
 
