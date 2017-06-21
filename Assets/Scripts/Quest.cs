4cd962d2a2bfbd8549a7c76eab5d200c6a04bf70
Chris de Castro <redsun@mac.com>
2017-06-21 19:34:05 -0400

    Added Quest class and Quest Database, works in game

---
+++ Assets/Scripts/Quest.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class Quest {
+	//quest name
+	//quest ID
+	//quest description
+	//quest Type
+	//quest Cost
+	//quest Item
+	//quest Target
+	public string questName;
+	public int questID;
+	public string questDescription;
+	public QuestType questType;
+	public int questCost;
+	public Item questItem;
+	public GameObject questTarget;
+
+	public enum QuestType {
+		Pay,
+		Get,
+		Give
+	}
+
+	public Quest(string name, int id, string description, QuestType type, int cost, Item item, GameObject obj) {
+		questName = name;
+		questID = id;
+		questDescription = description;
+		questCost = cost;
+		questType = type;
+		questItem = item;
+		questTarget = obj;
+	}
+
+	public Quest(){
+
+	}
+
+}
---
+++ Assets/Scripts/QuestDatabase.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestDatabase : MonoBehaviour {
+	public List<Quest> quests = new List<Quest> ();
+
+	void Start () {
+		quests.Add( new Quest("Pay Hydro", 1, "Hydro Bill is overdue", Quest.QuestType.Pay, 35, null, null));
+		quests.Add( new Quest("Pay Cable", 2, "Cable Bill is overdue", Quest.QuestType.Pay, 35, null, null));
+	}
+	
+	void Update () {
+		
+	}
+}
